e2deda4507577c372e2da40eab1da1fb361d5ed0
vtr363 <victor.m.rocha.vr@gmail.com>
2023-09-21 19:07:07 -0300

    Service atualizado

--- src/main/java/br/com/senac/service/AlunoService.java
+++ src/main/java/br/com/senac/service/AlunoService.java
@@ -10,18 +10,41 @@ import br.com.senac.repository.AlunoRepository;
 
 @Service
 public class AlunoService {
-
+	
 	@Autowired
 	AlunoRepository repo;
 	
 	public Aluno salvar(Aluno aluno) {
 		
 		 return repo.save(aluno);
-		
 	}
 	
-	public List<Aluno> buscarTodosAlunos(){
+	public List<Aluno> buscarTodosAlunos( ) {
 		return repo.findAll();
 	}
 	
+	public Aluno getAlunoById(Integer id) {
+		return repo.findById(id).orElse(null);
+	}
+	
+	public Boolean deleteAluno(Integer id) {
+		Aluno aluno = repo.findById(id).orElse(null);
+		if(aluno != null) {
+			repo.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+	
+	public Aluno updateAluno(Integer id, Aluno alunoAlterado) {
+		Aluno alunoBD = repo.findById(id).orElse(null);
+		if(alunoBD != null) {
+			alunoBD.setEmail(alunoAlterado.getEmail());
+			alunoBD.setNome(alunoAlterado.getNome());
+			alunoBD.setSobreNome(alunoAlterado.getSobreNome());
+			return repo.save(alunoBD);
+		}else {
+			return null;
+		}
+	}
 }
